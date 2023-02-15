8af36fb40c3a8104c237b08759715a5a8e94f8c3
Margarita <margaritakrasnauska@gmail.com>
2023-02-15 13:22:00 +0000

    Add project files.

---
+++ Enums/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public enum DaysOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thrusday,
+        Friday,
+        Saturday,
+        Sunday
+
+    }
+    class MyClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter a day of the week");
+            string YourDay = Console.ReadLine();
+            string ActualDay = DateTime.Now.DayOfWeek.ToString();
+            try
+            {
+                if (YourDay == ActualDay)
+                {
+                    Console.WriteLine("Today is : " + YourDay);
+                }
+                {
+                    if (YourDay != ActualDay)
+                        throw new ArgumentNullException();
+                }
+            }
+            catch (ArgumentNullException)
+
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
